7a0f7c6b3b4ced2273ca9c3d6c587a0d394a0782
anges-bai <45257069+anges-bai@users.noreply.github.com>
2020-01-29 21:47:58 +0800

    Create clas.java

---
+++ clas.java
@@ -0,0 +1,25 @@
+import java.lang.reflect.Constructor;
+
+class Person { 
+public Person() {
+
+}
+
+public Person(String name) {
+
+} 
+
+public Person(String name,int age) {
+
+}
+}
+
+public class Test { 
+public static void main(String[] args) { 
+Class<?> cls = Person.class ; // 取得类中的全部构造 
+Constructor<?>[] constructors = cls.getConstructors() ;
+for (Constructor<?> constructor : constructors) { 
+System.out.println(constructor);
+}
+}
+}
