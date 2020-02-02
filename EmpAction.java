309aacf0fed98a88b35acd568ff642b57cb6341a
anges-bai <45257069+anges-bai@users.noreply.github.com>
2020-02-02 18:52:52 +0800

    Create EmpAction.java

---
+++ EmpAction.java
@@ -0,0 +1,15 @@
+import www.bit.java.util.BeanOperation; 
+
+public class EmpAction {
+
+private Emp emp = new Emp() ;
+public void setValue(String value) throws Exception{ 
+
+BeanOperation.setBeanValue(this,value) ;
+
+}
+
+public Emp getEmp(){
+return emp ;
+}
+}
